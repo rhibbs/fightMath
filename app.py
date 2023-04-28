c46e1a23647c130511b65bc8b2fa5fbcc0c2bbdf
Ryan Hibbs <rh@panaton.com>
2023-04-28 08:57:37 -0600

    Added final results page, need to fix 'history_scraper' only returning first row of history added fighter records

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-from data.scraper import scrape_fighters
+from flask import Flask, render_template, request, redirect, url_for
+from data.scraper import scrape_fighters, history_scraper
 from flask_socketio import SocketIO
 
 app = Flask(__name__, template_folder='templates')
@@ -12,13 +12,25 @@ def index():
 async def result():
     fighter1 = request.json['fighter1']
     fighter2 = request.json['fighter2']
-    print(f"fighter1 Value is {fighter1}")
-    print(f"fighter2 Value is {fighter2}")
+    #print(f"fighter1 Value is {fighter1}")
+    #print(f"fighter2 Value is {fighter2}")
     fighter1_list = await scrape_fighters(fighter1)
     fighter2_list = await scrape_fighters(fighter2)
     #print(f"fighter1_list Value is {fighter1_list}")
     #print(f"fighter2 list is {fighter2_list}")
     return render_template('results.html', fighter1_list=fighter1_list, fighter2_list = fighter2_list)
+    #return redirect(url_for('common_fighters'))
+
+@app.route('/common_fighters', methods=["POST", "GET"])
+async def common_fighters():
+    fighter1 = request.form.get('checkbox_fighter1')
+    fighter2 = request.form.get('checkbox_fighter2')
+    #print(fighter1,fighter2)
+    fighter1_list = await history_scraper(fighter1)
+    fighter2_list = await history_scraper(fighter2)
+    #Scrape to find list of opponents for second fighter
+    #Compare two lists for common opponents
+    return render_template('common_ops.html', fighter1_list = fighter1_list, fighter2_list= fighter2_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
